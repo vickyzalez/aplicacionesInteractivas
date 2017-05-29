bffaf1811032e8cff33c7b6a183232b24c7e64c6
vickyzalez <gmariavictoria91@gmail.com>
2017-05-28 23:14:39 -0300

    Delete ProfesorPorClase.java

--- Gimnasio/src/modelo/ProfesorPorClase.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public class ProfesorParticular extends Profesor {
-
-	public ProfesorParticular(int dni, String nombre,String apellido, String domicilio, String mail) {
-		super(dni, nombre, apellido, domicilio, mail);
-	}
-	
-}
